cbe2e0fd5e7023a4f23ac539b03ad6d5fe193e4f
Minkyu222341 <qkralsrb2040@naver.com>
2022-07-14 14:31:32 +0900

    토큰오류

--- app.py
+++ app.py
@@ -1,70 +1,208 @@
-from flask import Flask, render_template, request, jsonify ,redirect
+import jwt as jwt
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('3.35.218.47', 27017, username="test", password="test")
 db = client.dbsparta_plus_week1
+
+SECRET_KEY = 'SPARTA'
+
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
+import datetime
+
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
+import hashlib
 
 
 @app.route('/')
 def home():
-    posts = list(db.board.find({}, {'_id': False}))
-    # print(len(posts))
-    print(posts)
-    return render_template('main.html',recipes_list=posts)
+   token_receive = request.cookies.get('mytoken')
+   try:
+       recipes_list = list(db.board.find({}, {'_id': False}))
+       payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+       user_info = db.member.find_one({"id": payload['id']})
+       return render_template('main.html', recipes_list=recipes_list, nickname=user_info["nick"])
+   except jwt.ExpiredSignatureError:
+       return redirect(url_for("home_main"))
+   except jwt.exceptions.DecodeError:
+       return redirect(url_for("home_main"))
+
+@app.route('/home_main')
+def home_main():
+   recipes_list = list(db.board.find({}, {'_id': False}))
+   return render_template('main.html', recipes_list=recipes_list, nickname="")
+
+
+@app.route('/main/<category>', methods=['GET'])
+def main(category):
+    # print(category)
+    if category == "all":
+        recipes_list = list(db.recipe.find({ }, {'_id': False}))
+    else:
+        recipes_list = list(db.recipe.find({'food': category}))
+        #print(recipes_list)
+    return render_template("main_"+category+".html", recipes_list=recipes_list)
 
 @app.route('/detail/<board_id>', methods=['GET'])
 def detail(board_id):
-    print(board_id)
-    board = list(db.board.find({'index': board_id}))
-    return render_template("detail.html", recipes=board)
+    token_receive = request.cookies.get('mytoken')
+    try:
+        board = list(db.board.find({'id': board_id}))
+        print(board)
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.member.find_one({"id": payload['id']})
+        return render_template("detail.html", recipes=board, nickname=user_info["nick"])
+    except jwt.exceptions.DecodeError:
+        return render_template("detail.html", recipes=board)
+
 
 @app.route('/add', methods=["GET"])
 def add_page():
     return render_template('add.html')
 
+
 @app.route('/update/<board_id>', methods=["GET"])
 def update_page(board_id):
-    write_data = list(db.board.find({'index': board_id}))
-    return render_template('update.html',write_data=write_data,index=board_id)
+    write_data = list(db.board.find({'id': board_id}))
+    return render_template('update.html', write_data=write_data, id=board_id)
+
 
 @app.route('/update', methods=["POST"])
 def updatePost():
     title_receive = request.form.get("title", type=str)
-    index_receive = request.form.get("index", type=str)
+    index_receive = request.form.get("id", type=str)
     url_receive = request.form.get("url", type=str)
     category_receive = request.form.get("category", type=str)
     content_receive = request.form.get("content", type=str)
     print(title_receive,index_receive)
-    db.board.update_one({'index':index_receive},{'$set':{'title':title_receive}})
-    db.board.update_one({'index': index_receive}, {'$set': {'url': url_receive}})
-    db.board.update_one({'index': index_receive}, {'$set': {'category': category_receive}})
-    db.board.update_one({'index': index_receive}, {'$set': {'content': content_receive}})
+    db.board.update_one({'id':index_receive},{'$set':{'title':title_receive}})
+    db.board.update_one({'id': index_receive}, {'$set': {'url': url_receive}})
+    db.board.update_one({'id': index_receive}, {'$set': {'category': category_receive}})
+    db.board.update_one({'id': index_receive}, {'$set': {'content': content_receive}})
 
     return redirect("/")
 
-
+#board - id , title, url , category , content , member_id , date
 @app.route("/add", methods=["POST"])
 def addPost():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    member_id = db.member.find_one({"id": payload['id']})
+
     posts = list(db.board.find({}))
-    index = str(len(posts)+1)
+    id = str(len(posts)+1)
     title_receive = request.form.get("title", type=str)
-    author_receive = request.form.get("author", type=str)
     url_receive = request.form.get("url", type=str)
     category_receive = request.form.get("category", type=str)
     content_receive = request.form.get("content", type=str)
-    print(title_receive,author_receive,url_receive,category_receive,content_receive)
+    date = "20:20:20"
     doc={
-        'index':index,
+        'id':id,
         'title':title_receive,
         'url': url_receive,
-        'author':author_receive,
         'category':category_receive,
-        'content':content_receive
+        'content':content_receive,
+        'member_id': member_id,
+        'date':date
     }
     db.board.insert_one(doc)
 
     return redirect("/")
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+#################################
+##  로그인을 위한 API            ##
+#################################
+
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    db.member.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
+
+    return jsonify({'result': 'success'})
+
+
+# [로그인 API]
+# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.member.find_one({'id': id_receive, 'pw': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=360)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+# [유저 정보 확인 API]
+# 로그인된 유저만 call 할 수 있는 API입니다.
+# 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
+# (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
+@app.route('/api/nick', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+
+    # try / catch 문?
+    # try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
+
+    try:
+        # token을 시크릿키로 디코딩합니다.
+        # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+
+        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
+        # 여기에선 그 예로 닉네임을 보내주겠습니다.
+        userinfo = db.member.find_one({'id': payload['id']}, {'_id': 0})
+        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
 if __name__ == '__main__':
